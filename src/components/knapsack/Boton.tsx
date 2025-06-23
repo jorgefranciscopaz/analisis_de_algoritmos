dd1b5aa7c95ef075d964eec75220ec1ba78da0e1
Gallowzz <134352607+Gallowzz@users.noreply.github.com>
2025-06-22 20:06:43 -0600

    Finalized Knapsack Implementation

--- src/components/knapsack/Boton.tsx
+++ src/components/knapsack/Boton.tsx
@@ -1,12 +1,17 @@
-type props = {
+type Props = {
   onClick: () => void;
-  texto: string
+  texto: string;
+  disabled?: boolean;
 }
 
-const Boton = ({onClick, texto}: props) => {
+const Boton = ({ onClick, texto, disabled = false }: Props) => {
   return (
-    <button className="cursor-pointer  text-white border-4 border-black rounded-4xl text-3xl font-bold" onClick={onClick}>
-      <div className="rounded-4xl px-6 py-2 text-2xl font-bold bg-linear-to-b from-green-700 to-green-500 border-4 border-green-500">
+    <button 
+      className={`text-white border-4 border-black rounded-4xl text-3xl font-bold ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`} 
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <div className={`rounded-4xl px-6 py-2 text-2xl font-bold ${disabled ? 'bg-gray-500' : 'bg-gradient-to-b from-green-700 to-green-500 hover:from-green-600 hover:to-green-400'} border-4 border-green-500`}>
         {texto}
       </div>
     </button>
